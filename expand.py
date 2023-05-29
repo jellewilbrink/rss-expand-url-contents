483a465e4881dd92ba3c6c83159cbd94d29a165a
jellewilbrink <49995469+jellewilbrink@users.noreply.github.com>
2023-05-30 00:47:53 +0200

    fix parameter not being used and clean up

--- expand.py
+++ expand.py
@@ -19,7 +19,7 @@ def expand_rss_to_url(
     Returns:
         - RSS feed output without the urls, but instead the text found on those pages.
     """
-    raw_xml = requests.get(feed_in).text
+    raw_xml = requests.get(feed_url).text
     req_counter = 1
 
     prev_hrefs = {}
@@ -45,16 +45,6 @@ def expand_rss_to_url(
                     site_soup = BeautifulSoup(site_xml, "html.parser")
 
                     element = site_soup.find(*bs4_find_all_params)
-                    # element="<p>hoi</p>"
-                    # x = ""
-                    # try:
-                    #     for s in element.findAll(string=True):
-                    #         x += s
-                    # except AttributeError:
-                    #     pass
-
-                    # element = x
-                    # print(element)
 
                     prev_hrefs[a["href"]] = copy.deepcopy(element)
 
@@ -67,19 +57,6 @@ def expand_rss_to_url(
     str_feed = str(feed_soup)
     str_feed = str_feed.replace("<description>", "<description><![CDATA[")
     str_feed = str_feed.replace("</description>", "]]></description>")
-
-    # new_feed_soup = BeautifulSoup(str(copy.deepcopy(feed_soup)), "xml")
-
-    # all_description_tags = new_feed_soup.findAll("description")
-
-    # for d in all_description_tags:
-    #     txt = ""
-    #     for s in d.findAll(string=True):
-    #         txt += s
-
-    #     copy_of_d = copy.deepcopy(d)
-    #     copy_of_d.string = txt
-    #     d.replace_with(copy_of_d)
 
     print(f"Number of requests made: {req_counter}")
     return str(str_feed)
