7ed5578a6accfff409d3dcbeae7fe0a9c6822a1d
jellewilbrink <49995469+jellewilbrink@users.noreply.github.com>
2023-05-29 19:22:35 +0200

    reduce number of requests made by storing them in a dict
    from 253 requests to 111 => over 2x improvement

--- expand.py
+++ expand.py
@@ -22,40 +22,50 @@ def expand_rss_to_url(
 
     Returns:
         - RSS feed output without the urls, but instead the text found on those pages.
-    """
-    # fp = feedparser.parse(feed_url)
+    """   
     raw_xml = requests.get(feed_in).text
+    req_counter = 1
 
-    count=0
-    feed_soup = BeautifulSoup(raw_xml, 'html.parser')
+    prev_hrefs = {}
+
+    feed_soup = BeautifulSoup(raw_xml, 'xml')
     all_str_tags = feed_soup.findAll(string=True)
+
     for txt in all_str_tags:
         
-        inner_feed_soup = BeautifulSoup(copy.deepcopy(txt), 'lxml')
+        inner_feed_soup = BeautifulSoup(copy.deepcopy(txt), 'html.parser')
         all_a_tags = inner_feed_soup.findAll('a', href=True)
 
         for a in all_a_tags:
 
             pattern = re.compile(pattern_to_expand)
             if pattern.match(a["href"]):
-                print(f"Parsing {a['href'] }")
+                if a["href"] in prev_hrefs:
+                    element = prev_hrefs[a["href"]]
+                else:
+                    # request the webpage
+                    print(f"Parsing {a['href'] }")
+                    
+                    req_counter +=1
+                    # site_xml = requests.get(a["href"]).text
+                    # site_soup = BeautifulSoup(site_xml, 'html.parser')
 
-                site_xml = requests.get(a["href"]).text
-                site_soup = BeautifulSoup(site_xml, 'html.parser')
-
-                element = site_soup.find(*bs4_find_all_params)
+                    # element = site_soup.find(*bs4_find_all_params)
+                    element = "hoi"
+                    prev_hrefs[a["href"]] = element
 
                 if element != None:
                     a.replace_with(element)
 
         txt.replace_with(inner_feed_soup)
 
+    print(f"Number of requests made: {req_counter}")
     return str(feed_soup)
 
 
 if __name__ == "__main__":
     feed_in = "https://anchor.fm/s/19b5cbd8/podcast/rss"
-    url_pattern="https://laernorsknaa.com/no_request*"
+    url_pattern="https://laernorsknaa.com/*"
     element_pattern=("div",{'class': 'wp-block-group__inner-container'})
     feed_out = expand_rss_to_url(feed_in, url_pattern, element_pattern)
 
